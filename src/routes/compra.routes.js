ae298011527f37417b83e157f9f85b314b3d79cd
BryanDev <bryanalejandrovo@ufps.edu.co>
2024-07-30 23:17:48 -0500

    Agregar filtro de compras rarito

--- src/routes/compra.routes.js
+++ src/routes/compra.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCompras, getComprasFilter, getComprasCount, createCompra, deleteCompra} from '../controllers/compra.controller.js'
+import { getCompras, getComprasFilter, getComprasFilterAesthetic, getComprasCount, createCompra, deleteCompra} from '../controllers/compra.controller.js'
 
 
 const router = Router()
@@ -7,6 +7,7 @@ const router = Router()
 
 router.get('/buys', getCompras)
 router.get('/buys/date', getComprasFilter)
+router.get('/buys/:inicio/:fin', getComprasFilterAesthetic);
 router.get('/buys/count', getComprasCount)
 router.post('/buys', createCompra)
 router.delete('/buys', deleteCompra)
